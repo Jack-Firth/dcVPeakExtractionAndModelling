b0f9ebec1f9a872067cebf226df323deb451a3f8
Jack Firth <jdavidfirth@gmail.com>
2021-04-12 16:02:46 +0100

    Added peak width at half height and electron estimation functionality

--- dcVPeakExtractionAndModelling.py
+++ dcVPeakExtractionAndModelling.py
@@ -18,7 +18,25 @@ def runLog(message, newRun=False): # Saves any posts to terminal into log.out fi
     f.write(str(message) + '\n')
     f.close()
     
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return(array[idx])
 
+def find_index(array, value):
+    returnIdx = 0
+    for i in range(0, len(array)):
+        if(array[i] == value):
+           returnIdx = i
+    return(returnIdx)
+
+def remove_from_array(array, value):
+    returnArray = np.zeros((len(array), 2))
+    for i in range(0,len(array)):
+        if(array[i,1] != value):
+            returnArray[i,:] = array[i,:]
+    return(returnArray)
+    
 def separatePeaks(inputdcV):   # Separates peaks by splitting dataset into 2 sections at the half-way point
     runLog("Separating forward and reverse scans...")
     if(inputdcV[2][1] < 0.001):
@@ -37,16 +55,12 @@ def separatePeaks(inputdcV):   # Separates peaks by splitting dataset into 2 sec
     #plt.xlabel(allXAxisLabel)
     #plt.ylabel(allYAxisLabel)
     #plt.legend(loc='upper left')
-    #plt.show(block=False)
+    #plt.show()
 
-    # toContinue = True
-    # while(toContinue == False):
-    #    if(input("Continue? (y/n): ") == 'y'):
-    #        toContinue = True
     return(allData)
 
 
-def fitPolynomialBaseline(fullDataset, echemRegion):    # Removes Echem region from raw data to then fit polynomials
+def fitPolynomialBaseline(fullDataset, echemRegion):    # Removes Echem region from  to then fit polynomials
     np.savetxt(".out/fulldataset.out", np.c_[fullDataset])
     np.savetxt(".out/echem.out", np.c_[echemRegion])
     fittingData = np.zeros((len(fullDataset), 2))
@@ -84,8 +98,8 @@ def plotPolys(plt, fullDataset, fittingData, echemRegion, polyQ=False):     # Ge
         polyArray = np.c_[polyArray, baseline_y]
         plt.plot(echemRegion[:, 0], echemRegion[:, 1], 'r-')
         plt.legend()
-        plt.set_xlim((0.95*min(echemRegion[:,0])), (1.05*max(echemRegion[:,0])))
-        plt.set_ylim((0.95*min(echemRegion[:,1])), (1.05*max(echemRegion[:,1])))
+        plt.set_xlim((0.7*min(echemRegion[:,0])), (1.3*max(echemRegion[:,0])))
+        plt.set_ylim((0.7*min(echemRegion[:,1])), (1.3*max(echemRegion[:,1])))
 
     if(polyQ == True):
         polyArray = np.delete(polyArray, 0, 1)
@@ -107,8 +121,8 @@ def selectCroppingRegion(xmin, xmax):   # Selects region of interest and calls p
         thisx = voltageUISelect[indmin: indmax]
         thisy = currentUISelect[indmin: indmax]
         zoomedLine.set_data(thisx, thisy)
-        ax2.set_xlim(thisx.min()-0.1, thisx.max()+0.1)
-        ax2.set_ylim(thisy.min()-1, thisy.max()+1)
+        ax2.set_xlim(0.7*(thisx.min()), 1.3*(thisx.max()))
+        ax2.set_ylim(0.7*(thisy.min()), 1.3*(thisy.max()))
         ax2.cla()
 
         echemData = np.c_[voltageUISelect, currentUISelect]
@@ -155,8 +169,7 @@ def findPeaks(dataset, echemData, baseline):    # Identifies peaks of interest b
     returnPeaks[:,0] = dataset[peaks, 0]
     returnPeaks[:,1] = dataset[peaks, 1]
     plt.plot(dataset[peaks, 0], dataset[peaks, 1], "x", markersize="10", label="Peaks")
-    plt.plot(dataset[:, 0], dataset[:, 1], label="Subtraction")
-    plt.plot(echemData[:,0], echemData[:,1], color='r', label="Raw Data")
+    plt.plot(echemData[:,0], echemData[:,1], color='r', label="")
     plt.xlim(0.9*min(dataset[:,0]), 1.1*max(dataset[:,0]))
     plt.legend()
     plt.show(block=False)
@@ -164,7 +177,13 @@ def findPeaks(dataset, echemData, baseline):    # Identifies peaks of interest b
     return(returnPeaks)
 
 
-def genCurrent(fParams, inputData):
+def elecCoverage(fParams, peakCurrent): # Estimates electrode coverage based on peak current
+    estCoverage = (4*peakCurrent*fParams["gasConstant"]*fParams["temp"])/(fParams["faradConstant"]**2*fParams["scanRate"]*fParams["elecArea"])  #right hand fraction is 1/4 at E0
+    estCoverage = estCoverage*0.000001
+    return(estCoverage)
+
+
+def genCurrent(fParams, inputData): # Generates model current based on peak positions
     returnCurrent = np.zeros((len(inputData), 2))
     constants = (fParams["nElec"]*fParams["nAppElec"]*(fParams["faradConstant"]**2)*fParams["scanRate"]*fParams["elecArea"]*fParams["elecCoverage"])/(fParams["gasConstant"]*fParams["temp"])
     for i in range(0,len(inputData[:,0])):
@@ -174,8 +193,37 @@ def genCurrent(fParams, inputData):
         modelledCurrent = constants*(topHalfOfFraction/bottomHalfOfFraction) * 1000000
         returnCurrent[i,0] = inputData[i,0]
         returnCurrent[i,1] = modelledCurrent
-    plt.plot(returnCurrent[:, 0], returnCurrent[:,1], label="Model")
-    plt.show()
+    return(returnCurrent)
+
+
+def peakWidthHalfHeight(identifiedPeaks, peakData):  # Takes peak current and returns the width at half height
+    for i in range(0,len(identifiedPeaks)):
+        #runLog(peakData)
+        nearestPeak = find_nearest(peakData[:,1], identifiedPeaks[1])
+        peakIdx = find_index(peakData[:,1], nearestPeak)   
+        lhs = peakData[:peakIdx]
+        rhs = peakData[peakIdx:]
+       
+        halfHeight = round((identifiedPeaks[1]/2), 2)
+        lhsHalfHeight = find_nearest(lhs[:,1], halfHeight)    
+        rhsHalfHeight = find_nearest(rhs[:,1], halfHeight)
+       
+        lhsIdx = find_index(lhs[:,1], lhsHalfHeight)
+        rhsIdx = find_index(rhs[:,1], rhsHalfHeight)
+
+        runLog(("   RHS", rhs[rhsIdx]))
+        runLog(("   LHS", lhs[lhsIdx]))
+
+        wAHH = round((rhs[rhsIdx, 0] - lhs[lhsIdx, 0]), 5)
+        wAHH = wAHH * 1000
+        runLog(("Width at Half-Height: ", wAHH, "mV"))
+
+        return(wAHH)
+
+
+def get_number_electrons(width, faradaicParameters):
+    nElectrons = (3.52*faradaicParameters['gasConstant']*faradaicParameters['temp'])/((width/1000)*faradaicParameters['faradConstant'])
+    return(nElectrons)
 
 def selectionUI(echemData, echemWindow=np.empty([]), fittingData=np.empty([])): # Front end of selection
     global fig
@@ -193,7 +241,7 @@ def selectionUI(echemData, echemWindow=np.empty([]), fittingData=np.empty([])): 
         "temp" : 278, 
         "nElec" : 1, 
         "nAppElec" : 1, 
-        "elecCoverage" : 650E-12,
+        "elecCoverage" : 0,
         "potential" : 0
     }
 
@@ -204,8 +252,8 @@ def selectionUI(echemData, echemWindow=np.empty([]), fittingData=np.empty([])): 
     plt.ylabel(allYAxisLabel)
     plt.xlabel(allXAxisLabel)
     ax2 = fig.add_subplot(122)
-    plt.xlabel(allXAxisLabel)
-
+    plt.xlabel(allXAxisLabel)    
+    
     voltageUISelect = echemData[:, 0]
     currentUISelect = echemData[:, 1]
 
@@ -221,41 +269,51 @@ def selectionUI(echemData, echemWindow=np.empty([]), fittingData=np.empty([])): 
                            'r-', label="Window")
     ax2.plot(voltageUISelect, currentUISelect, 'b--', alpha=0.3)
 
-    toContinue = False
-    while(toContinue == False):
-        try:
+    try:
             interestRegion = SpanSelector(ax, selectCroppingRegion, 'horizontal', useblit=True,
                                       rectprops=dict(alpha=0.5, facecolor='red'), span_stays=True)
             plt.legend()
             plt.show(block=False)
-        except Exception as e:
+    except Exception as e:
             runLog(e)
 
-        echemWindow = np.genfromtxt('.out/window.out', delimiter=" ")
-        runLog("read window file")
-        nPoly = int(input("Select Polynomial Order: "))
-        plt.close()
+    echemWindow = np.genfromtxt('.out/window.out', delimiter=" ")
+    runLog("read window file")
+    nPoly = int(input("Select Polynomial Order: "))
+    plt.close()
 
-        polyBaselines = np.genfromtxt('.out/polys.out', delimiter=" ")
-        baseline = np.zeros((len(echemData), 2))
-        (baseline[:, 0], baseline[:, 1]) = (
-            echemData[:, 0], polyBaselines[:, (nPoly-3)])
+    polyBaselines = np.genfromtxt('.out/polys.out', delimiter=" ")
+    baseline = np.zeros((len(echemData), 2))
+    (baseline[:, 0], baseline[:, 1]) = (
+        echemData[:, 0], polyBaselines[:, (nPoly-3)])
 
-        subtractedData = baselineSubtraction(echemData, baseline)
-        (regionOfInterest, shorterEchem) = focusData(subtractedData, echemWindow)
-        identifiedPeaks = findPeaks(regionOfInterest, shorterEchem, baseline)
-        runLog("Identified Peaks at:" )
-        runLog(identifiedPeaks)
+    subtractedData = baselineSubtraction(echemData, baseline)
+    (regionOfInterest, shorterEchem) = focusData(subtractedData, echemWindow)
+    identifiedPeaks = findPeaks(regionOfInterest, shorterEchem, baseline)
+    runLog("Identified Peaks at:" )
+    runLog(identifiedPeaks)
 
-        for i in range(0,len(identifiedPeaks - 1)):
-            faradaicParameters["potential"] = identifiedPeaks[i,0]
-            genCurrent(faradaicParameters, echemData)
+    modPeaks = np.zeros((len(echemData), len(identifiedPeaks)+1))
+    modPeaks[:, 0] = echemData[:,0]
 
-        # if(input("Continue? (y/n): ") == "y"):
-        toContinue = True
-        return(echemData, echemWindow, baseline)
+    for i in range(0,len(identifiedPeaks-1)):
+        faradaicParameters["elecCoverage"] = elecCoverage(faradaicParameters, identifiedPeaks[i,1])
+        runLog(("Peak at ", str(identifiedPeaks[i,0]), " estimated coverage: ", str(faradaicParameters["elecCoverage"]*1000000000000)))
+        faradaicParameters["potential"] = identifiedPeaks[i,0]
+        modCurrent = genCurrent(faradaicParameters, echemData)
+        modPeaks[:,i+1] = modCurrent[:,1]        
+    plt.plot(regionOfInterest[:,0], regionOfInterest[:,1], 'b--', label="Baseline Subtracted")
+    for i in range(0, len(identifiedPeaks)):
+        plt.plot(modPeaks[:,0], modPeaks[:,i+1], label=("Modelled Peak " + str(i+1)))
+    plt.legend()
+    plt.show()
 
-
+    for i in range(0, len(identifiedPeaks)):
+        runLog(("Modelled Peak: ", (i+1)))
+        width_at_half_height = peakWidthHalfHeight(identifiedPeaks[i,:], modPeaks[:,[0, i+1]])
+        number_of_Electrons = get_number_electrons(width_at_half_height, faradaicParameters)
+        runLog(("Number of Electrons: ", number_of_Electrons))
+    return(echemData, echemWindow, baseline)
 
 runLog("Running", True)
 # loads in input dcV file and separates into anodic and cathodic data
@@ -265,5 +323,5 @@ anodicData = allData[:, 0: 2]
 cathodicData = allData[:, 2:]
 
 (anodicData, anodicWindow, anodicBaseline) = selectionUI(anodicData)
-#(cathodicData, cathodicWindow, cathodicBaseline) = selectionUI(cathodicData)
+(cathodicData, cathodicWindow, cathodicBaseline) = selectionUI(cathodicData)
 
